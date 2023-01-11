6277212781d1c2db0b7ef698959a2ae8be1fda36
Pineda Prima <pinedaprima12@gmail.com>
2023-01-11 09:41:19 +0700

    Data Access Layer for Blog

---
+++ daoInSQLDudunBot/InSQLBlogDao.cs
@@ -0,0 +1,71 @@
+﻿using mdlDudunBot;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace daoInSQLDudunBot
+{
+    public class InSQLBlogDao : IBlogDao
+    {
+        readonly DudunBotEntities Db = new DudunBotEntities();
+        public Blog Create(Blog newBlog)
+        {
+            Db.Blogs.Add(newBlog);
+            Db.SaveChanges();
+
+            return newBlog;
+        }
+
+        public bool Delete(int id)
+        {
+            var data = GetById(id);
+            if (data == null)
+                return false;
+
+            Db.Blogs.Remove(data);
+            Db.SaveChanges();
+
+            return true;
+        }
+
+        public bool Update(Blog blog, int id)
+        {
+            var data = GetById(id);
+            if (data == null)
+                return false;
+
+            data.Author = blog.Author;
+            data.Title = blog.Title;
+            data.Subtitle = blog.Subtitle;
+            data.Content = blog.Content;
+            data.Image = blog.Image;
+            data.UpdatedAt = DateTime.Now.ToShortDateString();
+
+            Db.Entry(data).State = EntityState.Modified;
+            Db.SaveChanges();
+
+            return true;
+        }
+
+        public List<Blog> GetAll()
+        {
+            return Db.Blogs.ToList();
+        }
+
+        public Blog GetById(int id)
+        {
+            if (!Db.Blogs.Any(e => e.Id == id))
+                return null;
+
+            return Db.Blogs.FirstOrDefault(e => e.Id == id);
+        }
+
+        public int GetCount()
+        {
+            return Db.Blogs.Count();
+        }
+    }
+}
---
+++ mdlDudunBot/IBlogDao.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mdlDudunBot
+{
+    public interface IBlogDao
+    {
+        Blog Create(Blog newBlog);
+        List<Blog> GetAll();
+        Blog GetById(int id);
+        bool Update(Blog blog, int id);
+        bool Delete(int id);
+        int GetCount();
+    }
+}
